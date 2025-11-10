f0af5c42a2a43729962086507b2fd9a9cee96f7b
Kushall Sharma <97790774+KUSHALL433@users.noreply.github.com>
2025-11-10 17:19:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_groq import ChatGroq
 from langchain_core.prompts import PromptTemplate
 import streamlit as st
 from dotenv import load_dotenv
@@ -11,27 +12,21 @@ st.set_page_config(
     page_title="Summary Generator",
     page_icon="📝",
 )
+
 st.title('Text Summarizer.',anchor=False)
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
-if not GEMINI_API_KEY:
-    st.error("API Key not found. Please check your .env file.")
-else:
-    model = ChatGoogleGenerativeAI(model="gemini-2.5-flash", api_key=GEMINI_API_KEY)
+model = ChatGroq(model="llama-3.1-8b-instant")
 
-    template = PromptTemplate(
-        template='Give a 5 line summary on {text}',
-        input_variables=['text']
-    )
+template = PromptTemplate(template='Give a 5 line summary on {text}',
+        input_variables=['text'])
 
-    user_input = st.text_input("Enter a topic or large text to generate a summary")
+user_input = st.text_input("Enter a topic or large text to generate a summary")
 
-    if st.button('Submit') and user_input.strip():
-        prompt = template.invoke({'text': user_input})
-        result = model.invoke(prompt)
-        st.write(result.content)
-    # else:
-    #     st.warning("Please enter some text or topic.")
+if st.button('Submit') and user_input.strip():
+    prompt = template.invoke({'text': user_input})
+    result = model.invoke(prompt)
+    st.write(result.content)
+
 
 
 
